d0a9c08a6e2addea656c4e8ad1374b612d818062
menaTitan <msergeyous@gmail.com.com>
2022-01-13 23:37:41 -0600

    added model validation

--- Auth0Api/Auth0Api/Models/Quote.cs
+++ Auth0Api/Auth0Api/Models/Quote.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Auth0Api.Models
 {
     public class Quote
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
         public string Title { get; set; }
-
+        [Required]
+        [StringLength(20)]
         public string Author { get; set; }
-
+        [Required]
+        [StringLength(500)]
         public string Description { get; set; }
-
+        [Required]
         public string Type { get; set; }
-
+        [Required]
         public DateTime? CreatedAt { get; set; }
     }
 }
